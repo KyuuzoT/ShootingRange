ca638575695eb1588014bbd04827fe38b32f7f36
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2021-01-17 13:29:49 +0300

    Added character controll

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float sensivity = 3.0f;
+
+    private float minY = -45.0f;
+    private float maxY = 45.0f;
+
+    private float rotationX;
+    private float rotationY;
+
+    private Quaternion originalRotation;
+
+    private void Awake()
+    {
+        transform.GetComponent<Camera>().orthographic = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensivity;
+        var QuaternionX = Quaternion.AngleAxis(rotationX, Vector3.up);
+
+        rotationY += Input.GetAxis("Mouse Y") * sensivity;
+        rotationY = ClampAngle(rotationY, minY, maxY);
+        var QuaternionY = Quaternion.AngleAxis(rotationY, Vector3.left);
+
+        transform.rotation = originalRotation * QuaternionX * QuaternionY;
+    }
+
+    private float ClampAngle(float angle, float min, float max)
+    {
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/Player/Movement/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Vector3 transfer;
+    [SerializeField] private float speed = 3.0f;
+
+    void Update()
+    {
+        var cameraForward = transform.GetComponentInChildren<MouseLook>().transform.forward;
+        transform.forward = new Vector3(cameraForward.x, 0, cameraForward.z);
+
+        transfer = transform.forward * Input.GetAxis("Vertical");
+        transfer += transform.right * Input.GetAxis("Horizontal");
+
+        transform.position += transfer * speed * Time.deltaTime;
+    }
+}
